323f563bb326ac31fb273f20195bc113200eee22
tooblippe <tobie.nortje@gmail.com>
2017-05-19 14:53:16 +0200

    default network is now the example network in docs

--- pandapower_gui.py
+++ pandapower_gui.py
@@ -591,16 +591,21 @@ def splash(n=2):
 
 
 def create_dummy_collection():
-     # temp collections
     net = pp.create_empty_network()
-    b1 = pp.create_bus(net, 10, geodata=(5, 10))
-    b2 = pp.create_bus(net, 0.4, geodata=(5, 15))
-    b3 = pp.create_bus(net, 0.4, geodata=(0, 22))
-    b4 = pp.create_bus(net, 0.4, geodata=(8, 20))
-    pp.create_load(net, b4, 200)
-    pp.create_line(net, b2, b3, 2.0, std_type="NAYY 4x50 SE")
-    pp.create_line(net, b2, b4, 2.0, std_type="NAYY 4x50 SE")
-    pp.create_transformer(net, b1, b2, std_type="0.63 MVA 10/0.4 kV")
+    b1 = pp.create_bus(net, vn_kv=20., name="HV", geodata=(5, 30))
+    b2 = pp.create_bus(net, vn_kv=0.4, name="MV", geodata=(5, 28))
+    b3 = pp.create_bus(net, vn_kv=0.4, name="Load Bus", geodata=(5, 22))
+
+    #create bus elements
+    pp.create_ext_grid(net, bus=b1, vm_pu=1.02, name="Grid Connection")
+    pp.create_load(net, bus=b3, p_kw=100, q_kvar=50, name="Load")
+
+    #create branch elements
+    tid = pp.create_transformer(net, hv_bus=b1, lv_bus=b2, std_type="0.4 MVA 20/0.4 kV",
+                                name="Trafo")
+    pp.create_line(net, from_bus=b2, to_bus=b3, length_km=0.1, name="Line",
+                std_type="NAYY 4x50 SE")
+    
     return net
 
 if __name__ == '__main__':

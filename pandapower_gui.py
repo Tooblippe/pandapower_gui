bc3d90cf2068d409e57175f6e0dfbb6737ea93c0
tooblippe <tobie.nortje@gmail.com>
2017-05-22 13:18:19 +0200

    check vscode credentials

--- pandapower_gui.py
+++ pandapower_gui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) .....
+# Copyright (c) Tobie Nortje, Leon Thurner
 # All rights reserved. Use of this source code is governed
 # by a BSD-style license that can be found in the LICENSE file.
 # File created by Tobie Nortje ---
